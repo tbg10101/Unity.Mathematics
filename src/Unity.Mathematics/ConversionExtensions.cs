3157ccc80ef9233996d6717eecc7d01930393a7d
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2018-12-25 16:14:24 -0500

    adding explicit float3/Vector3 conversions

--- src/Unity.Mathematics/ConversionExtensions.cs
+++ src/Unity.Mathematics/ConversionExtensions.cs
@@ -35,6 +35,21 @@ namespace Unity.Mathematics {
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float2 toFloat2(this Vector2 f) {
+			return float2(f.x, f.y);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float3 toFloat3(this Vector3 f) {
+			return float3(f.x, f.y, f.z);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float4 toFloat4(this Vector4 f) {
+			return float4(f.x, f.y, f.z, f.w);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static double2 toDouble2(this Vector2 f) {
 			return double2(f.x, f.y);
 		}
@@ -47,6 +62,21 @@ namespace Unity.Mathematics {
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static double4 toDouble4(this Vector4 f) {
 			return double4(f.x, f.y, f.z, f.w);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector2 toVector2 (this float2 d) {
+			return new Vector2(d.x, d.y);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector3 toVector3 (this float3 d) {
+			return new Vector3(d.x, d.y, d.z);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector4 toVector4 (this float4 d) {
+			return new Vector4(d.x, d.y, d.z, d.w);
 		}
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
